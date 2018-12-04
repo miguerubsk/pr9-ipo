d572f9e4beabdad994045d11ddfc187d93caf214
Miguel González García <mgg00112@red.ujaen.es>
2018-12-04 18:50:10 +0100

    Codigo reestructurado

--- src/main.java
+++ src/main.java
@@ -1,9 +1,5 @@
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -21,23 +17,16 @@ import java.util.ArrayList;
 public class main {
     
     
-    public static void leerfichero(ArrayList lista, String fichero) throws FileNotFoundException, IOException{
-        FileReader f = new FileReader(fichero);
-        BufferedReader b = new BufferedReader(f);
-        String cadena;
-        while((cadena = b.readLine())!=null) {
-            lista.add(cadena);
-    }
-    };
+
 
     /**
      * @param args the command line arguments
      * @throws java.io.IOException
      */
     public static void main(String[] args) throws IOException {
-        ArrayList<String> listaidiomas = new ArrayList<>();
-        leerfichero(listaidiomas, "idiomas.txt");
-        ventana vent = new ventana(listaidiomas);
+        
+        
+        ventana vent = new ventana();
         vent.setVisible(true);
     }
     
--- src/ventana.java
+++ src/ventana.java
@@ -1,6 +1,10 @@
 
 import java.awt.Container;
 import java.awt.FlowLayout;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import javax.swing.*;
 import java.util.ArrayList;
 
@@ -12,21 +16,43 @@ import java.util.ArrayList;
 
 /**
  *
- * @author macosx
+ * @author miguerubsk
  */
 public class ventana extends JFrame {
+    
+    public static void leerfichero(ArrayList lista, String fichero) throws FileNotFoundException, IOException{
+        FileReader f = new FileReader(fichero);
+        BufferedReader b = new BufferedReader(f);
+        String cadena;
+        while((cadena = b.readLine())!=null) {
+            lista.add(cadena);
+    }
+    };
 
-    public ventana(ArrayList<String> listaidiomas) {
+    public ventana() throws IOException {
         super("Internacionalización");
         setSize(400, 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Container cp = getContentPane();
-        cp.setLayout(new FlowLayout());
-        JLabel etiqueta = new JLabel("Idioma: ");
-        JButton boton = new JButton("Aplicar"); 
+        
+        
+        ArrayList<String> listaidiomas = new ArrayList<>();
+        leerfichero(listaidiomas, "idiomas.txt");
+        
+        
         JComboBox idioma;
         idioma = new JComboBox();
         idioma.setModel(new DefaultComboBoxModel(listaidiomas.toArray()));
+        
+        idioma.getSelectedItem();
+        String rutaTextos = idioma.getSelectedItem()+".txt";
+        
+        ArrayList<String> textos = new ArrayList<>();
+        leerfichero(textos, rutaTextos);
+        
+        Container cp = getContentPane();
+        cp.setLayout(new FlowLayout());
+        JLabel etiqueta = new JLabel(textos.get(0));
+        JButton boton = new JButton(textos.get(1)); 
         cp.add(etiqueta);
         cp.add(idioma);
         cp.add(boton);
